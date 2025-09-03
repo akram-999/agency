f4e5acdf96c63f22dd0f024841f4a62244b1a406
akram ahil <akram.ahil123@gmail.com>
2025-09-03 11:32:50 +0100

    hero v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
   return (
     <div className='dark:bg-gray-900 relative'>
       <Navbar theme={theme} setTheme={setTheme}/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="relative isolate px-6 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-20">
+          <div className=" hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="flex justify-center gap-2 relative rounded-full px-3 py-1 text-sm/6 text-gray-400 border border-gray-300">
+                <div className="flex -space-x-1 ">
+                <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="inline-block size-6 rounded-full " />
+                <img src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="inline-block size-6 rounded-full " />
+                <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="" className="inline-block size-6 rounded-full " />
+                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="inline-block size-6 rounded-full " />
+                </div>
+                Trusted by over 1000+ clients
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance sm:text-7xl dark:text-white">
+              Turning imagination into Digital impact
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+              Creating meaningful connections and turning ideas into reality.
+            </p>
+            
+          </div>
+          <div className='mt-7'>
+            <img src="/group.jpg" alt="agency" className='rounded-xl'/>
+        </div>
+        </div>
+       
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
