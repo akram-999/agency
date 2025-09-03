1b522c7d506fba1518cdabdd62e2da202952d8f1
akram ahil <akram.ahil123@gmail.com>
2025-09-03 14:05:46 +0100

    clients v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Clients from './components/Clients'
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
   return (
     <div className='dark:bg-gray-900 relative'>
       <Navbar theme={theme} setTheme={setTheme}/>
       <Hero/>
+      <Clients />
     </div>
   )
 }
